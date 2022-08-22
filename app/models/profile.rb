c5f52dbc9007659cf2f350c8bbdd05998becd64e
rubensy <86684948+rubensy@users.noreply.github.com>
2022-08-22 16:47:41 +0200

    added right version of profile model and started setting devise gem

---
+++ app/models/profile.rb
@@ -0,0 +1,7 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :address, presence: true
+end
