9865add2dd432c395ba806e3aafa4f2c706e88ff
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-22 16:51:08 +0200

    added validations to Bear model

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -1,3 +1,6 @@
 class Bear < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :personality, presence: true, length: { minimum: 3 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
