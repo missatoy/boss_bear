23762e49162500e25348819021296f36e44f3eac
rubensy <86684948+rubensy@users.noreply.github.com>
2022-08-22 17:03:09 +0200

    all models fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_142320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bears", force: :cascade do |t|
+    t.string "name"
+    t.string "personality"
+    t.text "description"
+    t.integer "price"
+    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bears_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "bear_id", null: false
+    t.date "date_start"
+    t.date "date_finish"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bear_id"], name: "index_bookings_on_bear_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bears", "users"
+  add_foreign_key "bookings", "bears"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "profiles", "users"
 end
