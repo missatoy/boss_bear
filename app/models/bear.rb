3ce0e9c977a33bc3f51f6d7f529fdf1752076521
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-22 17:54:32 +0200

    created bears controller

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -1,5 +1,6 @@
 class Bear < ApplicationRecord
   belongs_to :user
+  has_one :profile, through: :user
   validates :name, presence: true, length: { minimum: 2 }
   validates :personality, presence: true, length: { minimum: 3 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
