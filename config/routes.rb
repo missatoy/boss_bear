a371a7719dae71921dd23a622f337e47f9e5c9af
Daniel Guntermann <d.guntermann@me.com>
2022-08-23 14:38:54 +0200

    Created bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :bears
+  resources :bears do
+    resources :bookings, only: :create
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "profiles/:id", to: "profiles#show"
   # Defines the root path route ("/")
