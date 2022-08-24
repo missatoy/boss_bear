8f4d375837b8aee4af0577b1588e040664bd7f00
rubensy <86684948+rubensy@users.noreply.github.com>
2022-08-24 15:25:59 +0200

    fixed show

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr"
-
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    console.log('test')
-    flatpickr(".datepicker", {});
   }
 }
