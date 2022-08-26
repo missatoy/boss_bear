ac56128395ca332950e858c326b65977b40ac582
rubensy <86684948+rubensy@users.noreply.github.com>
2022-08-26 09:47:18 +0200

    finished and centered the lend a bear form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  has_one :profile, through: :user
+  has_one :profile, through: :user, dependent: :destroy
   belongs_to :bear
   validates :date_start, presence: true
   validates :date_finish, presence: true
